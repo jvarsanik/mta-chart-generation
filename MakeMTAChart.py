370f4d64e089f7ac971e9b4cdc1cbd2b6edbf91b
Jon - MBP <jon@atlas5d.com>
2018-11-28 00:02:01 -0500

    Multiple plots across levels
    Make one plot for all top level tasks on one plot, then another plot for each of those tasks for their subtasks, down tot he desired level.

--- MakeMTAChart.py
+++ MakeMTAChart.py
@@ -20,12 +20,13 @@ from matplotlib.patches import Polygon
 import numpy as np
 
 # User variables
-PLOT_LEVEL = 4
+TOP_LEVEL_TO_PLOT = 3 # In team gantt, these are the major levels (level 1 is the project level)
+BOTTOM_LEVEL_TO_PLOT = 4
 DNAME_IN = 'output'
 FNAME_IN = 'TechTeamCurrent.csv'
 
-PLOT_TITLE = 'Tech Team Milestones'
-TASKS_TO_INCLUDE = ['1.1.1']
+MAIN_PLOT_TITLE = 'Tech Team Overall'
+TASKS_TO_INCLUDE = ['1.1']
 TASKS_TO_EXCLUDE = []
 
 # FUnctions!
@@ -40,10 +41,10 @@ def isSubtask(task_number, super_task_number):
     this_val =  task_number.startswith(super_task_number)
     return this_val
 
-def checkIfShouldInclude(task_number):
+def checkIfShouldInclude(task_number, tasks_to_include, tasks_to_exclude):
     # First, check if this task is explicitly included
-    if TASKS_TO_INCLUDE:
-        for task in TASKS_TO_INCLUDE:
+    if tasks_to_include:
+        for task in tasks_to_include:
             if isSubtask(task_number, task):
                 return True
         
@@ -51,8 +52,8 @@ def checkIfShouldInclude(task_number):
         return False
     
     # Next, check if the task is exploicitly excluded
-    if TASKS_TO_EXCLUDE:
-        for task in TASKS_TO_EXCLUDE:
+    if tasks_to_exclude:
+        for task in tasks_to_exclude:
             if isSubtask(task_number, task):
                 return False
         
@@ -112,216 +113,259 @@ def num(s):
     except ValueError:
         return float(s)
 
-
-## Script is here
-# Check for CURRENT.csv
-data_fname = os.path.join(DNAME_IN, FNAME_IN)
-
-if not os.path.isfile(data_fname):
-    print('No CURRENT.csv file at location: ', data_fname)
-
-# get the headers to get the column labels
-csvfile = open(data_fname)
-reader = csv.reader(csvfile)
-
-#  get the header row:
-row1 = next(reader)
-dates_calculated_str = row1[2:]
-dates_calculated = convertListToDates(dates_calculated_str)
-
-# Initialize the holders
-task_objs = []
-latest_end_dates = []
-
-# loop through file and populate the holders
-for line in reader:
-
-    this_task_number = line.pop(0)
-    this_task_name = line.pop(0)
-
-    if not this_task_number:
-        continue
-
-    this_level = num(calculateLevel(this_task_number))
-
-    # Skip level 1 (this is just the project header...)
-    if this_level < 2:
-        continue
-
-    these_end_dates = convertListToDates(line)
-
-    # Filter the dates to only plot dates with data
-    end_dates_filtered1, calculated_dates_filtered1 = filterDatesToOnlyPlotDatesWithData(these_end_dates, dates_calculated)
-
-    if not end_dates_filtered1:
-        continue
-
-    # filter dates so the lines stop when they hit their completion date
-    end_dates_filtered, calculated_dates_filtered = filterEndDatesToNotGoPastCalculatedDate(end_dates_filtered1, calculated_dates_filtered1)
-
-    #update the data holder
-    this_obj = {}
-    this_obj['task_number'] = this_task_number
-    this_obj['task_name'] = this_task_name
-    this_obj['end_dates'] = end_dates_filtered
-    this_obj['latest_end_date'] = end_dates_filtered[-1]
-    this_obj['calculated_dates'] = calculated_dates_filtered
-    this_obj['level'] = this_level
-    this_obj['top_parent'] = calculateTopParentTask(this_task_number)
-
-    # put this in the list
-    task_objs.append(this_obj)
-
-    # keep list of latest end dates, for sorting later
-    latest_end_dates.append(this_obj['latest_end_date'])
+def make_plot_for_tasks(task_objs, plot_level, tasks_to_include, tasks_to_exclude, plot_title):
+    #  Now plot the level you want (will plot this level and above (lower level ( 1st is highest level))
+    fig, ax = plt.subplots()
+    n = datetime.now()
+    this_month = datetime(year=n.year, month=n.month, day=1)
+    max_date = datetime(year=1, month=1, day=1)
+    min_date = this_month
 
 
-# close the csv file (bad practivce, shoud do in t with...)
-csvfile.close()
+    # Get max and min dates for this plot 
+    # Go through each task and add to plot
+    for task in task_objs:
+        this_level = task['level']
+        this_number = task['task_number']
+        these_end_dates = task['end_dates']
+        these_calculated_dates = task['calculated_dates']
 
+        # Check level
+        if this_level > plot_level:
+            continue
 
-# Sort the array, to plot them in the right order (longest on top(plotted first))
-inds = range(len(latest_end_dates))
-sorted_inds = [x for _,x in sorted(zip(latest_end_dates, inds), reverse=True)]
+        # Check if shoudl include
+        if not checkIfShouldInclude(this_number, tasks_to_include, tasks_to_exclude):
+            continue
 
-task_objs = [task_objs[i] for i in sorted_inds]
+        # % update max and min 
+        if max(these_end_dates) > max_date:
+            max_date = max(these_end_dates)
 
-#  Now plot the level you want (will plot this level and above (lower level ( 1st is highest level))
-fig, ax = plt.subplots()
-n = datetime.now()
-this_month = datetime(year=n.year, month=n.month, day=1)
-max_date = this_month
-min_date = this_month
+        if max(these_calculated_dates) > max_date:
+            max_date = max(these_calculated_dates)
 
-if min(dates_calculated) < min_date:
-    min_date = min(dates_calculated)
+        if min(these_end_dates) < min_date:
+            min_date = min(these_end_dates)
 
-max_date = max(latest_end_dates)
+        if min(these_calculated_dates) < min_date:
+            min_date = min(these_calculated_dates)
 
-# find range to get amount to shift overlapping plots (for aesthetics)
-this_plot_range = max_date-min_date
-this_plot_epsilon = this_plot_range / 100
+    # find range to get amount to shift overlapping plots (for aesthetics)
+    this_plot_range = max_date-min_date
+    this_plot_epsilon = this_plot_range / 100
 
-# reset max date
-max_date = min_date
+    # store list of datapoints to detect collisions (so we can offset overlapping plots)
+    data_points = {}
 
-# store list of datapoints to detect collisions (so we can offset overlapping plots)
-data_points = {}
+    # Go through each task and add to plot
+    for task in task_objs:
+        this_level = task['level']
+        this_number = task['task_number']
+        this_name = task['task_name']
+        these_end_dates = task['end_dates']
+        these_calculated_dates = task['calculated_dates']
 
-# Go through each task and add to plot
-for task in task_objs:
-    this_level = task['level']
-    this_number = task['task_number']
-    this_name = task['task_name']
-    these_end_dates = task['end_dates']
-    these_calculated_dates = task['calculated_dates']
+        # Check level
+        if this_level > plot_level:
+            continue
 
-    # Check level
-    if this_level > PLOT_LEVEL:
-        continue
+        # Check if shoudl include
+        if not checkIfShouldInclude(this_number, tasks_to_include, tasks_to_exclude):
+            continue
+        
+        # Check for collisions (overlapping data points) - remove a little time until there is no collision
+        # Remove time so the ordering of the legend entries is correct (we plot from top down...)
+        for ind, this_calculated_date in enumerate(these_calculated_dates):
+            while data_points and (this_calculated_date in data_points) and (these_end_dates[ind] in data_points[this_calculated_date]):
+                #print('Collision for ', this_name, ' on ', this_calculated_date, 'at ' , these_end_dates[ind], ' - adding some time')
+                these_end_dates[ind] = these_end_dates[ind] - this_plot_epsilon
 
-    # Check if shoudl include
-    if not checkIfShouldInclude(this_number):
-        continue
+            #  Add to all data points store to look for more collisions
+            if this_calculated_date in data_points:
+                data_points[this_calculated_date].append(these_end_dates[ind])
+            else:
+                data_points[this_calculated_date] = [these_end_dates[ind]]
 
-    # % update max and min 
-    if max(these_end_dates) > max_date:
-        max_date = max(these_end_dates)
+        #print('Plotting ', this_name, ': ',   convertListOfDatesToString(these_end_dates))
 
-    if max(these_calculated_dates) > max_date:
-        max_date = max(these_calculated_dates)
-
-    if min(these_end_dates) < min_date:
-        min_date = min(these_end_dates)
-
-    # Check for collisions (overlapping data points) - remove a little time until there is no collision
-    # Remove time so the ordering of the legend entries is correct (we plot from top down...)
-    for ind, this_calculated_date in enumerate(these_calculated_dates):
-        while data_points and (this_calculated_date in data_points) and (these_end_dates[ind] in data_points[this_calculated_date]):
-            print('Collision for ', this_name, ' on ', this_calculated_date, 'at ' , these_end_dates[ind], ' - adding some time')
-            these_end_dates[ind] = these_end_dates[ind] - this_plot_epsilon
-
-        #  Add to all data points store to look for more collisions
-        if this_calculated_date in data_points:
-            data_points[this_calculated_date].append(these_end_dates[ind])
+        # add to plot (plotting single points differently)
+        if len(these_calculated_dates) < 2:
+            ax.plot(these_calculated_dates, these_end_dates, 'o', label=this_name)
         else:
-            data_points[this_calculated_date] = [these_end_dates[ind]]
+            ax.plot(these_calculated_dates, these_end_dates, label=this_name)
 
-    print('Plotting ', this_name, ': ',   convertListOfDatesToString(these_end_dates))
+    # Do plot formatting
 
-    # add to plot (plotting single points differently)
-    if len(these_calculated_dates) < 2:
-        ax.plot(these_calculated_dates, these_end_dates, 'o', label=this_name)
+    # Set the limits (set to line up with months (if range is large enough)
+    if min_date + relativedelta(months=4) < max_date:
+        max_date = datetime(max_date.year, max_date.month, 1) + relativedelta(months=1)
+        min_date = datetime(min_date.year, min_date.month, 1)
+
     else:
-        ax.plot(these_calculated_dates, these_end_dates, label=this_name)
+        max_date = max_date + timedelta(days=1)
+        min_date = min_date - timedelta(days=1)
 
-# Do plot formatting
+    ax.set_xlim(min_date, max_date)
+    ax.set_ylim(min_date, max_date)
 
-# Set the limits (set to line up with months (if range is large enough)
-if min_date + relativedelta(months=4) < max_date:
-    max_date = datetime(max_date.year, max_date.month, 1) + relativedelta(months=1)
-    min_date = datetime(min_date.year, min_date.month, 1)
+    # set the aspect ratio 
+    ax.set_aspect('equal')
 
-else:
-    max_date = max_date + timedelta(days=1)
-    min_date = min_date - timedelta(days=1)
+    # adjust the grid
+    # every monday
+    # mondays = WeekdayLocator(MONDAY)
+    # months = MonthLocator(range(1, 13), bymonthday=1, interval=3)
+    # monthsFmt = DateFormatter("%b '%y")
 
-ax.set_xlim(min_date, max_date)
-ax.set_ylim(min_date, max_date)
+    ax.autoscale_view()
+    #ax.xaxis.grid(False, 'major')
+    #ax.xaxis.grid(True, 'minor')
+    ax.grid(True)
 
-# set the aspect ratio 
-ax.set_aspect('equal')
+    # set the axes to be below other elements (so the patch hides the grid)
+    ax.set_axisbelow(True)
 
-# adjust the grid
-# every monday
-# mondays = WeekdayLocator(MONDAY)
-# months = MonthLocator(range(1, 13), bymonthday=1, interval=3)
-# monthsFmt = DateFormatter("%b '%y")
+    # Plot the triangle to cover the grid in the lower half
+    # triangle_points = np.array([[min_date, min_date], [max_date, min_date], [max_date, max_date]])
+    start = mdates.date2num(min_date)
+    end = mdates.date2num(max_date)
+    triangle_points = np.array([[start, start], [end, start], [end, end]])
+    # Make triangle - set zorder to 5 to put above all lines (default zorder 2, patch default is 1)
+    triangle = Polygon(triangle_points, closed=True, color='white', zorder=5) 
+    ax.add_patch(triangle)
 
-ax.autoscale_view()
-#ax.xaxis.grid(False, 'major')
-#ax.xaxis.grid(True, 'minor')
-ax.grid(True)
+    # PLot the target line
+    target_line_dates = [min_date, max_date]
+    ax.plot(target_line_dates, target_line_dates, color='black', zorder=10)
 
-# set the axes to be below other elements (so the patch hides the grid)
-ax.set_axisbelow(True)
+    # # put x ticks at top
+    # ax.xaxis.tick_top()
+    # ax.xaxis.set_label_position('top') 
 
-# Plot the triangle to cover the grid in the lower half
-# triangle_points = np.array([[min_date, min_date], [max_date, min_date], [max_date, max_date]])
-start = mdates.date2num(min_date)
-end = mdates.date2num(max_date)
-triangle_points = np.array([[start, start], [end, start], [end, end]])
-# Make triangle - set zorder to 5 to put above all lines (default zorder 2, patch default is 1)
-triangle = Polygon(triangle_points, closed=True, color='white', zorder=5) 
-ax.add_patch(triangle)
+    # adjust the labels
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    ax.format_ydata = mdates.DateFormatter('%Y-%m-%d')
+    fig.autofmt_xdate()
 
-# PLot the target line
-target_line_dates = [min_date, max_date]
-ax.plot(target_line_dates, target_line_dates, color='black', zorder=10)
+    #  Show the legend
+    ax.legend()
 
-# # put x ticks at top
-# ax.xaxis.tick_top()
-# ax.xaxis.set_label_position('top') 
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 
-# adjust the labels
-ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
-ax.format_ydata = mdates.DateFormatter('%Y-%m-%d')
-fig.autofmt_xdate()
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-#  Show the legend
-ax.legend()
+    # put a title and axis labels
+    ax.set_title(plot_title)
+    ax.set_xlabel('Date report generated')
+    ax.set_ylabel('Projected completion date')
 
-# Shrink current axis by 20%
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    # show the plot
+    #plt.show()
 
-# Put a legend to the right of the current axis
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-# put a title and axis labels
-plt.title(PLOT_TITLE)
-plt.xlabel('Date report generated')
-plt.ylabel('Projected completion date')
+# Import the tasks from the csv file into task_objs, and a list of the top level tasks
+def import_tasks_from_csv(full_fname_in, tasks_to_include, tasks_to_exclude, top_level_to_plot):
+    # Check for CURRENT.csv
+    data_fname = full_fname_in
 
-# show the plot
+    if not os.path.isfile(data_fname):
+        print('No file at location: ', data_fname)
+
+    # get the headers to get the column labels
+    csvfile = open(data_fname)
+    reader = csv.reader(csvfile)
+
+    #  get the header row:
+    row1 = next(reader)
+    dates_calculated_str = row1[2:]
+    dates_calculated = convertListToDates(dates_calculated_str)
+
+    # Initialize the holders
+    task_objs = []
+    latest_end_dates = []
+    top_level_tasks = []
+
+    # loop through file and populate the holders
+    for line in reader:
+
+        this_task_number = line.pop(0)
+        this_task_name = line.pop(0)
+
+        if not this_task_number:
+            continue
+
+        this_level = num(calculateLevel(this_task_number))
+
+        # Skip level 1 (this is just the project header...) and anything above top_level_to_plot
+        if (this_level < top_level_to_plot) | (this_level < 2):
+            continue
+
+        these_end_dates = convertListToDates(line)
+
+        # Filter the dates to only plot dates with data
+        end_dates_filtered1, calculated_dates_filtered1 = filterDatesToOnlyPlotDatesWithData(these_end_dates, dates_calculated)
+
+        if not end_dates_filtered1:
+            continue
+
+        # filter dates so the lines stop when they hit their completion date
+        end_dates_filtered, calculated_dates_filtered = filterEndDatesToNotGoPastCalculatedDate(end_dates_filtered1, calculated_dates_filtered1)
+
+        #update the data holder
+        this_obj = {}
+        this_obj['task_number'] = this_task_number
+        this_obj['task_name'] = this_task_name
+        this_obj['end_dates'] = end_dates_filtered
+        this_obj['latest_end_date'] = end_dates_filtered[-1]
+        this_obj['calculated_dates'] = calculated_dates_filtered
+        this_obj['level'] = this_level
+        this_obj['top_parent'] = calculateTopParentTask(this_task_number)
+
+        # Check if shoudl include
+        if not checkIfShouldInclude(this_task_number, tasks_to_include, tasks_to_exclude):
+            continue
+
+        # put this in the list
+        task_objs.append(this_obj)
+
+        # keep list of latest end dates, for sorting later
+        latest_end_dates.append(this_obj['latest_end_date'])
+
+        # keep a list of top-level parent tasks
+        if this_level == top_level_to_plot:
+            top_level_tasks.append(this_obj)
+
+    # close the csv file (bad practivce, shoud do in t with...)
+    csvfile.close()
+
+
+    # Sort the array, to plot them in the right order (longest on top(plotted first))
+    inds = range(len(latest_end_dates))
+    sorted_inds = [x for _,x in sorted(zip(latest_end_dates, inds), reverse=True)]
+
+    task_objs = [task_objs[i] for i in sorted_inds]
+
+    return task_objs, top_level_tasks
+
+
+## SCRIPT IS HERE
+
+# import the data form the csv
+full_fname_in = os.path.join(DNAME_IN, FNAME_IN)
+task_objs, top_level_tasks = import_tasks_from_csv(full_fname_in, TASKS_TO_INCLUDE, TASKS_TO_EXCLUDE, TOP_LEVEL_TO_PLOT)
+
+# Make one plot for the highest level, then go through each top level task and plot the subtasks.
+print('Making overall plot at level: ', TOP_LEVEL_TO_PLOT)
+make_plot_for_tasks(task_objs, TOP_LEVEL_TO_PLOT, TASKS_TO_INCLUDE, TASKS_TO_EXCLUDE, MAIN_PLOT_TITLE)
+
+# Now, for each top level task, make a plot of its sub tasks
+for this_top_level_task in top_level_tasks:
+    print('making plot for: ', this_top_level_task['task_name'], ' - task number: ', this_top_level_task['task_number'])
+    make_plot_for_tasks(task_objs, BOTTOM_LEVEL_TO_PLOT, [this_top_level_task['task_number']], [], this_top_level_task['task_name'])
+
+# show the plots
 plt.show()
